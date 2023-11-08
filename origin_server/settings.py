fad1f315caf68cec15df968f9fe020188f5677e1
David Aragon Rocha <124726565+davidsito-004@users.noreply.github.com>
2023-11-08 12:55:13 -0600

    Update settings.py

--- origin_server/settings.py
+++ origin_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3nme2a-6c(+_+s3z9=fwo-f&*laq3c+6hnx6_(s$rfo&tzd_+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["web-production-c5fb.up.railway.app", "brunitoproject.lat", "www.brunitoproject.lat"]
 
