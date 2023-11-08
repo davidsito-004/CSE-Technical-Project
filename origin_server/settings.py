ebd2c5e2b9b1d1447bc60dcd40e1ba13a3b8d460
David Aragon Rocha <124726565+davidsito-004@users.noreply.github.com>
2023-11-08 13:21:53 -0600

    Update settings.py

--- origin_server/settings.py
+++ origin_server/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["web-production-c5fb.up.railway.app", "brunitoproject.lat", "www.brunitoproject.lat"]
 
-CSRF_TRUSTED_ORIGINS=["web-production-c5fb.up.railway.app", "brunitoproject.lat", "www.brunitoproject.lat"]
+CSRF_TRUSTED_ORIGINS=["https://web-production-c5fb.up.railway.app", "brunitoproject.lat", "www.brunitoproject.lat"]
 
 
 # Application definition
